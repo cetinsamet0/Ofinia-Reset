5c78ee8e268651b6c0480807ce2ffd4505180b43
cetinsamet0 <cetinsamet659@gmail.com>
2025-06-02 14:44:06 +0300

    Boş Proje Oluşturuldu.

---
+++ Forms/main_menu.cs
@@ -0,0 +1,15 @@
+namespace ofinia_reset
+{
+    public partial class main_menu : Form
+    {
+        public main_menu()
+        {
+            InitializeComponent();
+        }
+
+        private void main_menu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
